e6c75868ed7d2e99365259d1505a5c2fe13d3c59
Supa <supa.codes@gmail.com>
2026-04-08 14:01:22 +0300

    redlib: disable

--- flake.nix
+++ flake.nix
@@ -26,7 +26,6 @@
     stylix.url = "github:nix-community/stylix/release-25.11";
     spicetify-nix.url = "github:Gerg-L/spicetify-nix";
     technorino.url = "git+https://github.com/2547techno/technorino?submodules=1";
-    redlib.url = "github:0Supa/redlib/prod";
     uploader-basic.url = "github:0Supa/uploader-basic";
 
     yt-dlp-git = {
