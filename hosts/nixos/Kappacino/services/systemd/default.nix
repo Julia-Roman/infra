76956cb2e69f23472164812a73f18222b4b85ac0
Supa <git@supa.sh>
2026-04-16 18:54:38 +0000

    umami: fix service

--- hosts/nixos/Kappacino/services/systemd/default.nix
+++ hosts/nixos/Kappacino/services/systemd/default.nix
@@ -42,9 +42,31 @@ in
 
     rustlog = mkService "./rustlog" "${home}/git/rustlog" [ ];
 
-    umami = mkService "PORT=1700 node .next/standalone/server.js" "${home}/git/umami" [
-      pkgs.nodejs_22
-    ];
+    umami =
+      mkService
+        ''
+          PRISMA_SCHEMA_ENGINE_BINARY="${pkgs.prisma-engines}/bin/schema-engine"
+          PRISMA_QUERY_ENGINE_BINARY="${pkgs.prisma-engines}/bin/query-engine"
+          PRISMA_QUERY_ENGINE_LIBRARY="${pkgs.prisma-engines}/lib/libquery_engine.node"
+          PRISMA_FMT_BINARY="${pkgs.prisma-engines}/bin/prisma-fmt"
+          PATH="$PWD/node_modules/.bin/:$PATH"
+          NODE_ENV=production
+          PORT=1700
+          node node_modules/next/dist/bin/next start
+        ''
+        "${home}/git/umami"
+        (
+          with pkgs;
+          [
+            nodejs_22
+            pnpm
+            yarn
+            vips
+            python3
+            prisma-engines
+            openssl
+          ]
+        );
 
     mediamtx = mkService "./mediamtx" "${home}/git/mediamtx" [ pkgs.ffmpeg-full ];
 
