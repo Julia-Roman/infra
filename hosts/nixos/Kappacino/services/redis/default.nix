84c0d144e7e02bcf46d6d6f608681bbc1293dd9b
Supa <git@supa.sh>
2026-04-16 07:31:16 +0000

    redis: remove roles_lookup server

--- hosts/nixos/Kappacino/services/redis/default.nix
+++ hosts/nixos/Kappacino/services/redis/default.nix
@@ -7,10 +7,6 @@
       enable = true;
       port = 6379;
     };
-    "roles_lookup" = {
-      enable = true;
-      port = 6380;
-    };
     "umami" = {
       enable = true;
       port = 6381;
