db34c6c31005e34fe8c9798a012868fceb81f41e
Supa <supa@juliaelena.ro>
2026-05-31 16:06:46 +0300

    caddy: update hash

--- hosts/nixos/Kappacino/services/caddy/default.nix
+++ hosts/nixos/Kappacino/services/caddy/default.nix
@@ -12,7 +12,7 @@
       plugins = [
         "github.com/mholt/caddy-ratelimit@v0.1.0"
       ];
-      hash = "sha256-TKwIOw9sU3j28J/gFMFCnR5IlBA1N17Rx6T1glZ23ug=";
+      hash = "sha256-JBDPuNQ3wDLnoNlWjts9u4MYLnqcNULW7/j2Ybhim7M=";
     };
 
     virtualHosts = {
