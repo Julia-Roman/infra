1402a97a25ce2d07f5b236eadc98a7aca5a8706d
Supa <supa@juliaelena.ro>
2026-06-02 20:03:50 +0300

    grafana: set security.secret_key

--- hosts/nixos/Kappacino/services/grafana/default.nix
+++ hosts/nixos/Kappacino/services/grafana/default.nix
@@ -5,6 +5,7 @@
   services.grafana = {
     enable = true;
     settings = {
+      security.secret_key = "$__file{/etc/secrets/grafana/secret_key}";
       server = {
         http_addr = "0.0.0.0";
         http_port = 3133;
