7c03b106e7be32d1a47348d7ff725e1f1551bc17
Supa <supa@juliaelena.ro>
2026-08-26 04:42:23 +0000

    users: remove default values already set

--- hosts/nixos/homesrv/users.nix
+++ hosts/nixos/homesrv/users.nix
@@ -42,44 +42,6 @@
         linger = true;
       };
 
-      omuljake = {
-        isNormalUser = true;
-        openssh.authorizedKeys.keys = [
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINYXj7DSfF7bzZxN8cV86m0v+58ZwfxbWfx4hDu7kxOT omuljake"
-        ];
-        createHome = true;
-        extraGroups = [ "fivem" ];
-        home = "/home/omuljake";
-      };
-
-      goku = {
-        isNormalUser = true;
-        openssh.authorizedKeys.keys = [
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJgt0vqaXWSmTaIjI1q/AoWLoImK/Ecb8pa2G/q+2hbQ goku"
-        ];
-        createHome = true;
-        extraGroups = [ "fivem" ];
-        home = "/home/goku";
-      };
-
-      chimichanga = {
-        isNormalUser = true;
-        openssh.authorizedKeys.keys = [ ];
-        createHome = true;
-        extraGroups = [ "fivem" ];
-        home = "/home/chimichanga";
-      };
-
-      fui = {
-        isNormalUser = true;
-        openssh.authorizedKeys.keys = [
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKdu/8WZGXWasf/7hjAWI4uwi3KaBIUjURlNC7A9s34V fui"
-        ];
-        createHome = true;
-        extraGroups = [ "fivem" ];
-        home = "/home/fui";
-      };
-
       qbittorrent = {
         isSystemUser = true;
         group = "media";
@@ -87,18 +49,48 @@
         home = "/var/lib/qbittorrent";
       };
 
+      jellyfin.extraGroups = [ "media" ];
+      caddy.extraGroups = [ "www" ];
+
+      omuljake = {
+        isNormalUser = true;
+        openssh.authorizedKeys.keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINYXj7DSfF7bzZxN8cV86m0v+58ZwfxbWfx4hDu7kxOT omuljake"
+        ];
+        extraGroups = [ "fivem" ];
+      };
+
+      goku = {
+        isNormalUser = true;
+        openssh.authorizedKeys.keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJgt0vqaXWSmTaIjI1q/AoWLoImK/Ecb8pa2G/q+2hbQ goku"
+        ];
+        extraGroups = [ "fivem" ];
+      };
+
+      chimichanga = {
+        isNormalUser = true;
+        openssh.authorizedKeys.keys = [
+
+        ];
+        extraGroups = [ "fivem" ];
+      };
+
+      fui = {
+        isNormalUser = true;
+        openssh.authorizedKeys.keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKdu/8WZGXWasf/7hjAWI4uwi3KaBIUjURlNC7A9s34V fui"
+        ];
+        extraGroups = [ "fivem" ];
+      };
+
       zonian = {
         isNormalUser = true;
         openssh.authorizedKeys.keys = [
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIADNDlNyn1HGSOpBUBR4lalWNZoiFzAQrnKaRH7dxFS/ zonianmidian@gmail.com"
         ];
-        createHome = true;
-        home = "/home/zonian";
         linger = true;
       };
-
-      jellyfin.extraGroups = [ "media" ];
-      caddy.extraGroups = [ "www" ];
     };
   };
 }
