85f12c15a2a7a00378962cf4d19c093d282e7a24
Supa <supa@juliaelena.ro>
2026-09-14 05:30:03 +0000

    fivem: set env file

--- hosts/nixos/homesrv/services/fivem.nix
+++ hosts/nixos/homesrv/services/fivem.nix
@@ -14,6 +14,8 @@
       Restart = "always";
       RestartSec = 3;
       WorkingDirectory = "/home/fivem/artifacts";
+      EnvironmentFile = "/etc/fivem.env";
+      ExecStartPre = "/bin/sh /home/fivem/artifacts/txData/QBCoreFramework_9EAABC.base/generate-env-cfg.sh";
       ExecStart = "/bin/sh run.sh";
     };
     wantedBy = [ "multi-user.target" ];
